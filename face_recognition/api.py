c72f4ecd5df969f2925648283f1a20e575854152
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-03-04 19:43:21 +0100

    Face_encodings

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -64,9 +64,33 @@ def _raw_face_locations():
     pass
 
 
-def _trim_css_to_bounds():
+def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="small"):
+    """
+    Given an image, return the 128-dimension face encoding for each face in the image.
+
+    :param face_image: The image that contains one or more faces :param known_face_locations: Optional - the bounding
+    boxes of each face if you already know them.
+    :param num_jitters: How many times to re-sample the face when
+    calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+    :param model: Optional - which model to use. "large" or "small" (default) which only returns 5 point but is faster.
+    :return: A list of 128-dimensional face encodings (one for each face in the image)
+    """
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
+    return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for
+            raw_landmark_set in raw_landmarks]
+
+
+def _rect_to_css(rect):
     pass
 
 
-def _rect_to_css():
+def _css_to_rect(css):
+    pass
+
+
+def _trim_css_to_bounds(css, image_shape):
+    pass
+
+
+def face_distance(face_encodings, face_to_compare):
     pass
