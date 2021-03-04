0d7a134e6bc856a6bba89915fac36d67595d4eff
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-03-04 19:59:17 +0100

    format file

--- face_recognition/__init__.py
+++ face_recognition/__init__.py
@@ -1,3 +1,4 @@
 __version__ = '0.1.0'
 
-from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, \
+    face_distance
