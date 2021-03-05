56c51694a2256fa426c744419bba58440a79c0e6
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-03-05 15:03:31 +0100

    format file

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -64,7 +64,8 @@ def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="
     """
     Given an image, return the 128-dimension face encoding for each face in the image.
 
-    :param face_image: The image that contains one or more faces :param known_face_locations: Optional - the bounding
+    :param face_image: The image that contains one or more faces
+    :param known_face_locations: Optional - the bounding
     boxes of each face if you already know them.
     :param num_jitters: How many times to re-sample the face when
     calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
