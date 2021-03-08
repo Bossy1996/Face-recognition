aa7ef3b0b4ec4e683a1c7e443c1b723371475ff7
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-03-08 12:02:19 +0100

    Finished api

--- face_recognition/api.py
+++ face_recognition/api.py
@@ -32,7 +32,7 @@ def load_image_file(file, mode='RGB'):
     Loads an image file (.jpg, png, etc...) into numpy array
     :param file: image file name or file object to load
     :param mode: format to convert the image to. Only 'RGB' (8-bit RGB, 3 channels) and 'L' (black and white) are supported.
-    :return: image content as a numpy array
+    :return: image content as a numpy array.
     """
     img = PIL.Image.open(file)
 
