54f13a3cff2aed3ff9a807ddd5bed21a4560944b
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-03-11 23:05:29 +0100

    Real time face detection tool file created

---
+++ face_recognition/Real-time_face-detection.py
@@ -0,0 +1,10 @@
+import os
+import re
+import face_recognition.api as face_recognition
+import multiprocessing
+import sys
+import itertools
+
+
+def live_detection(image_to_check, model, upsample):
+    pass
